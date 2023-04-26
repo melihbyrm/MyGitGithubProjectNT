628188398b6212bda4029f82ec0265f84f7352a1
melih <melih32fb23@hotmail.com>
2023-04-26 22:55:40 +0300

    mlh1

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -8,5 +8,8 @@ public class AtmProject {
         //version3
         System.out.println("Git ogrenmek cok kolay");
         //version4
+
+        System.out.println("melih branchi kodlarini yazdi.");
+        //version mlh1
     }
 }
